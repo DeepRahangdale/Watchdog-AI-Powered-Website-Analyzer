c4cd32a61eba5637787f178b167e1462837939cf
DeepRahangdale <deeprahangdale2003@gmai.com>
2024-06-29 01:17:45 +0530

    Update!!

--- backend/server.js
+++ backend/server.js
@@ -67,12 +67,6 @@ async function fetchWebsiteText(url) {
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--no-first-run',
-        '--no-zygote',
-        '--single-process', // <- this one doesn't work in Windows
-        '--disable-gpu'
       ],
       headless: true
     });
